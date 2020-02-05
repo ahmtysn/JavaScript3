6d9b21934763a7e006bcde4c29bf0dea40324130
ahmtysn <ahmtysn14@gmail.com>
2020-02-05 19:35:49 +0100

    project improved

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -1,60 +1,61 @@
-"use strict";
+'use strict';
 
-const root = document.getElementById("root");
-const repoContainer = document.querySelector(".repo-container");
-const contributorsContainer = document.querySelector(".contributors-container");
-const ulRepo = createAndAppend("ul", repoContainer);
-const ulContributor = createAndAppend("ul", contributorsContainer);
+const root = document.getElementById('root');
+const repoContainer = document.querySelector('.repo-container');
+const contributorsContainer = document.querySelector('.contributors-container');
+const ulRepo = createAndAppend('ul', repoContainer);
+const ulContributor = createAndAppend('ul', contributorsContainer);
 
 function main(url) {
   fetch(url)
     .then(res => res.json())
     .then(repos => {
-      const select = document.querySelector("header select");
-
+      const select = document.querySelector('header select');
       const sortedRepo = repos.sort((curRepo, nextRepo) =>
-        curRepo.name.localeCompare(nextRepo.name)
+        curRepo.name.localeCompare(nextRepo.name),
       );
       renderRepoDetails(sortedRepo[0], ulRepo);
       sortedRepo.forEach(repo => {
         let repoName = repo.name.toLowerCase();
-        createAndAppend("option", select, {
+        createAndAppend('option', select, {
           value: repo.name,
-          text: repoName
+          text: repoName,
         });
       });
-
-      select.addEventListener("change", selectOptions);
-      function selectOptions() {
-        fetch(
-          `https://api.github.com/repos/HackYourFuture/${select.value}/contributors`
-        )
-          .then(res => res.json())
-          .then(data => {
-            ulContributor.innerHTML = "";
-            ulRepo.innerHTML = "";
-            contributorsContainer.classList.remove("noneDisplay");
-            repos
-              .filter(repo => repo.name === select.value)
-              .forEach(repo => renderRepoDetails(repo, ulRepo));
-            renderContributor(data, ulContributor);
-          })
-          .catch(err => console.error(err));
-      }
+      select.addEventListener('change', () => {
+        repoSelection(select, repos);
+      });
     })
     .catch(err => {
-      createAndAppend("div", root, {
+      createAndAppend('div', root, {
         text: err.message,
-        class: "alert-error"
+        class: 'alert-error',
       });
     });
+}
+
+function repoSelection(select, repos) {
+  fetch(
+    `https://api.github.com/repos/HackYourFuture/${select.value}/contributors`,
+  )
+    .then(res => res.json())
+    .then(data => {
+      ulContributor.innerHTML = '';
+      ulRepo.innerHTML = '';
+      contributorsContainer.classList.remove('noneDisplay');
+      repos
+        .filter(repo => repo.name === select.value)
+        .forEach(repo => renderRepoDetails(repo, ulRepo));
+      renderContributor(data, ulContributor);
+    })
+    .catch(err => console.error(err));
 }
 
 function createAndAppend(name, parent, options = {}) {
   const elem = document.createElement(name);
   parent.appendChild(elem);
   Object.entries(options).forEach(([key, value]) => {
-    if (key === "text") {
+    if (key === 'text') {
       elem.textContent = value;
     } else {
       elem.setAttribute(key, value);
@@ -64,21 +65,21 @@ function createAndAppend(name, parent, options = {}) {
 }
 
 function renderRepoDetails(repo, ulRepo) {
-  const li = createAndAppend("li", ulRepo);
-  const table = createAndAppend("table", li);
-  const titles = ["Repository:", "Description:", "Forks:", "Updated:"];
-  const keys = ["name", "description", "forks", "updated_at"];
+  const li = createAndAppend('li', ulRepo);
+  const table = createAndAppend('table', li);
+  const titles = ['Repository:', 'Description:', 'Forks:', 'Updated:'];
+  const keys = ['name', 'description', 'forks', 'updated_at'];
 
   for (let i = 0; i < titles.length; ++i) {
-    let tr = createAndAppend("tr", table);
-    createAndAppend("th", tr, { text: titles[i] });
-    if (i) createAndAppend("td", tr, { text: repo[keys[i]] });
+    let tr = createAndAppend('tr', table);
+    createAndAppend('th', tr, { text: titles[i] });
+    if (i) createAndAppend('td', tr, { text: repo[keys[i]] });
     else {
-      const td = createAndAppend("td", tr);
-      createAndAppend("a", td, {
+      const td = createAndAppend('td', tr);
+      createAndAppend('a', td, {
         href: repo.html_url,
         text: repo.name,
-        target: "_blank"
+        target: '_blank',
       });
     }
   }
@@ -86,25 +87,25 @@ function renderRepoDetails(repo, ulRepo) {
 
 function renderContributor(data, ulContributor) {
   for (let i = 0; i < data.length; ++i) {
-    const li = createAndAppend("li", ulContributor, {
-      class: "contribution-container"
+    const li = createAndAppend('li', ulContributor, {
+      class: 'contribution-container',
     });
-    createAndAppend("img", li, {
+    createAndAppend('img', li, {
       src: data[i].avatar_url,
-      class: "contribution-avatar"
+      class: 'contribution-avatar',
     });
-    createAndAppend("a", li, {
+    createAndAppend('a', li, {
       text: data[i].login,
       href: data[i].html_url,
-      target: "_blank",
-      class: "contribution-name"
+      target: '_blank',
+      class: 'contribution-name',
     });
-    createAndAppend("span", li, {
+    createAndAppend('span', li, {
       text: data[i].contributions,
-      class: "contribution-number"
+      class: 'contribution-number',
     });
   }
 }
 
-const hyfURL = "https://api.github.com/orgs/HackYourFuture/repos?per_page=100";
+const hyfURL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
 window.onload = () => main(hyfURL);
